206486e01ac7791c2e2ee91bb54b6ba04c0caedb
Billy Angermeier <william.r.angermeier@gmail.com>
2018-04-23 19:47:22 -0400

    updated title and url

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ $('form').submit(function(e) {
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.foursquare.com/v2/venues/explore?client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323&ll=35.863243800000006%2C%20-78.6227746",
+  "url": "https://api.foursquare.com/v2/venues/explore&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323&ll=35.863243800000006%2C%20-78.6227746",
   "method": "GET",
   "headers": {
     "cache-control": "no-cache",
