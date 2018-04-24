44a9cb78e81948e6a53f806d837148d3cdd98c23
Billy Angermeier <william.r.angermeier@gmail.com>
2018-04-24 19:11:09 -0400

    fix ajax issues for foursquare

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ $('form').submit(function(e) {
 var settings = {
   "async": true,
   "crossDomain": true,
-  "url": "https://api.foursquare.com/v2/venues/explore&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323&ll=35.863243800000006%2C%20-78.6227746",
-  "method": "GET",
-  "headers": {
-    "cache-control": "no-cache",
-    "postman-token": "8e1717ed-cf30-3a7c-2e6a-4baa2d378806"
+  "url": "https://api.foursquare.com/v2/venues/search?ll=35.8,-78.6&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323",
+  "type": "GET",
+  "part": "response"
+  
   }
-}
 
-$.ajax(settings).done(function (response) {
-  console.log(response);
+
+$.ajax(settings).done(function (data) {
+  console.log(data);
+  for(i=0;i<data.response.venues.length;i++){
+    $(`.results`).append(`<div class = 'end'>${data.response.venues[i].name}</div>`);
+  }
 });
 });
  
