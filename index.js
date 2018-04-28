60d057e1414cf8a488dfd64f32764646945557bd
Billy Angermeier <william.r.angermeier@gmail.com>
2018-04-28 18:18:45 -0400

    added weatherbit api

--- index.js
+++ index.js
@@ -1,44 +1,65 @@
+function makeWeather(lat, lon, locale) {
+  var weatherSettings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&city=Raleigh%2CNC",
+    "method": "GET"
+  };
 
 
-function makeSearch(location,locale) {
+  if (lat&&lon) {
+    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&lat="+lat+"&lon="+lon+"&units=I";
+  } else {
+    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&city="+locale+"&units=I"
+    }
+
+  $.ajax(weatherSettings).done(function (response) {
+    console.log(response);
+  })
+}
+
+function makeSearch(location, locale) {
   var settings = {
     "async": true,
     "crossDomain": true,
     "url": "https://api.foursquare.com/v2/venues/search?near=" + locale + "&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323",
     "type": "GET",
     "part": "response"
-  } 
-  
-  if(location){
-    settings.url="https://api.foursquare.com/v2/venues/search?ll=" + location + "&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323"
-    
-  } else{
-    settings.url="https://api.foursquare.com/v2/venues/search?near=" + locale + "&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323"
+  }
+
+  if (location) {
+    settings.url = "https://api.foursquare.com/v2/venues/search?ll=" + location + "&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323"
+
+  } else {
+    settings.url = "https://api.foursquare.com/v2/venues/search?near=" + locale + "&client_id=1R2KOI5C0RYHU5IPYW4WC32EROLJRQFJOEG5YPERNLMAL3GU&client_secret=VOLTI1HDXQJ1FSIRNCW3MA41WE4RHP3FBWAQGR22ZY4QWDEU&query=disc%20golf&v=20180323"
   }
 
   $.ajax(settings).done(function (data) {
     console.log(data);
     $(`.results`).empty();
-    
+
     for (i = 0; i < data.response.venues.length; i++) {
-      if(data.response.venues[i].categories[0].name==="Disc Golf")
-      $(`.results`).append(`<div class = 'end'>${data.response.venues[i].name}</div>`);
+      if (data.response.venues[i].categories.length>0 && data.response.venues[i].categories[0].name === "Disc Golf")
+        $(`.results`).append(`<div class = 'end'>${data.response.venues[i].name}</div>`);
     }
   });
 }
 
-function showPosition(position){
-  var location = position.coords.latitude+","+position.coords.longitude;
+function showPosition(position) {
+  var location = position.coords.latitude + "," + position.coords.longitude;
+  var lat = position.coords.latitude;
+  var lon = position.coords.longitude;
   makeSearch(location, null);
+  makeWeather(lat, lon, null);
 }
 
-function receiveError(error){
+function receiveError(error) {
   console.log(error);
 }
 
 $(function () {
   if (navigator.geolocation); {
-    navigator.geolocation.getCurrentPosition(showPosition,receiveError);
+    navigator.geolocation.getCurrentPosition(showPosition, receiveError);
   }
 })
 
@@ -47,7 +68,8 @@ $(function () {
 $('form').submit(function (event) {
   event.preventDefault();
   let locale = $('#searchbar').val();
-  makeSearch(null,locale)
+  makeSearch(null, locale)
+  makeWeather(null, null, locale);
 
 
 });
