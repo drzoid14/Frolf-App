5129798f3fa06f5f0decca9fb81c9a8246c90a69
Billy Angermeier <william.r.angermeier@gmail.com>
2018-04-30 12:26:05 -0400

    added weather data to html, added styling

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
+function setBackground(response){
+  var current = response.data[0].weather.description;
+  console.log(current);
+  $(`.weather`).append(`${current}`);
+  if(current.toLowerCase().includes("thunderstorm")){
+    $('body').css('background-image','url(http://vunature.com/wp-content/uploads/2016/11/sky-rain-storm-thunderstorm-nature-clouds-lightning-wallpaper-for-desktop-full-size-free-download.jpg)');
+  } else{
+    if(current.toLowerCase().includes("clear")){
+      $('body').css('background-image','url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ6Iubalw-RYjADFRtECVne0RA0O2lrbZcF0Ildr7SfUNEOoA1UKg)');
+    } else{
+      if(current.toLowerCase().includes("rain")||current.toLowerCase().includes("drizzle")||current.toLowerCase().includes("precipitation")){
+        $('body').css('background-image','url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTecF7UU7E9mGwmwf4Jmhdy2U9sK2Rgxs_ezbSMn1lmv1u7toQ0)');
+      } else{
+        if(current.toLowerCase().includes("snow")||current.toLowerCase().includes("flurries")){
+          $('body').css('background-image','url(https://www.social-graphics.com/twitter-images/snowy-background.jpg)');
+        } else{
+          if(current.toLowerCase().includes("fog")||current.toLowerCase().includes("haze")||current.toLowerCase().includes("smoke")||current.toLowerCase().includes("sand")||current.toLowerCase().includes("mist")){
+            $('body').css('background-image','url(http://www.triciamccallum.com/writer/wp-content/uploads/Pier-in-Fog-870x400.jpg)');
+          } else{
+              if(current.toLowerCase().includes("cloud")){
+                $('body').css('background-image','url(http://www.desktopwallpaperhd.net/wallpapers/1/0/desktop-oregon-background-joseph-partly-sky-cloudy-18624.jpg)');
+              }
+
+}}}}}}
+
 function makeWeather(lat, lon, locale) {
   var weatherSettings = {
     "async": true,
@@ -7,15 +32,23 @@ function makeWeather(lat, lon, locale) {
   };
 
 
-  if (lat&&lon) {
-    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&lat="+lat+"&lon="+lon+"&units=I";
+  if (lat && lon) {
+    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&lat=" + lat + "&lon=" + lon + "&units=I";
   } else {
-    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&city="+locale+"&units=I"
-    }
+    weatherSettings.url = "https://api.weatherbit.io/v2.0/current?key=2b4af8efa8cf40a590c218af015143a4&city=" + locale + "&units=I"
+  }
 
   $.ajax(weatherSettings).done(function (response) {
     console.log(response);
+
+
+    $(`.weather`).html(`<div class="temp">${response.data[0].app_temp} F</div>`);
+    $(`.weather`).append(`<div class="wind">${response.data[0].wind_spd} mph</div>`);
+    setBackground(response);
   })
+
+
+
 }
 
 function makeSearch(location, locale) {
@@ -39,7 +72,7 @@ function makeSearch(location, locale) {
     $(`.results`).empty();
 
     for (i = 0; i < data.response.venues.length; i++) {
-      if (data.response.venues[i].categories.length>0 && data.response.venues[i].categories[0].name === "Disc Golf")
+      if (data.response.venues[i].categories.length > 0 && data.response.venues[i].categories[0].name === "Disc Golf")
         $(`.results`).append(`<div class = 'end'>${data.response.venues[i].name}</div>`);
     }
   });
